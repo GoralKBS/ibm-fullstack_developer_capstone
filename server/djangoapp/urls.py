5ca6dd8b9fb77df88400245d924174cf6e5b118a
GoralKBS <gorlapuru1@gmail.com>
2024-08-04 13:24:44 -0400

    Edited in Module3 Edit2_final of Capstone

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
@@ -14,11 +15,11 @@ urlpatterns = [
     #path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
-    #path for registration
-    path(route='register', view=views.registration, name='register'),
-
     # path for dealer reviews view
 
     # path for add a review view
+
+    #path for get_cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
